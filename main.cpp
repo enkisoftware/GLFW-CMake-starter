ea61edd7d2da37d427fdd45670f940b27f71b8aa
Hailey <haa40@sfu.ca>
2023-10-01 17:07:22 -0700

    triangle renders

--- main.cpp
+++ main.cpp
@@ -1,38 +1,56 @@
 #include <GLFW/glfw3.h>
+#include <bits/stdc++.h>
 
-int main(void)
-{
+int main() {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+    const std::pair<int, int> windowDim(640, 480);
+    window = glfwCreateWindow(windowDim.first, windowDim.second, "Window name goes here", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    // Make Context
+    glfwMakeContextCurrent(window);
+
+    // Loop
+    while (!glfwWindowShouldClose(window)) {
+        float ratio;
+        int width, height;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = (float)width / (float)height;
+
+        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        // I don't understand below,
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
 
-        /* Poll for and process events */
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        // Swap front/back buffers
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    exit(EXIT_SUCCESS);
 }
