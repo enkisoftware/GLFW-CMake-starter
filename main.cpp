145dc89f53878f594783f23b6dca4246169f0f77
Hailey <haa40@sfu.ca>
2023-10-02 17:51:20 -0700

    drawing gradient

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     }
 
     const std::pair<int, int> windowDim(640, 480);
-    window = glfwCreateWindow(windowDim.first, windowDim.second, "Window name goes here", NULL, NULL);
+    window = glfwCreateWindow(windowDim.first, windowDim.second, "asdf", NULL, NULL);
     if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
@@ -18,31 +18,29 @@ int main() {
     // Make Context
     glfwMakeContextCurrent(window);
 
+    // Print something
+    std::cout << "Starting program" << std::endl;
+
+    float ratio;
+    int width, height;
+
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = (float)width / (float)height;
+    std::cout << "framebuffer sizes: width " << width << ", height " << height << std::endl;
+
     // Loop
     while (!glfwWindowShouldClose(window)) {
-        float ratio;
-        int width, height;
-
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = (float)width / (float)height;
-
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // I don't understand below,
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
+        // Begin mode: draw points
+        glBegin(GL_POINTS);
+        for (int i = 0; i < 2*width; i++) {
+            for (int j = 0; j < 2*height; j++) {
+                glColor3f((float)j/(float)height,(float)i/(float)width,1.f);
+                glVertex2f((float)j/(float)height-1.f,(float)i/(float)width-1.f);
+            }
+        }
         glEnd();
 
         // Swap front/back buffers
